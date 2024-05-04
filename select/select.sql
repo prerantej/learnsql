b376fcfe8e7a753b22b373f07655445ef4f84864
prerantej <prerantejkvr@gmail.com>
2024-05-04 19:50:14 +0530

    code snippet for select statement

---
+++ select/select.sql
@@ -0,0 +1,24 @@
+--SELECT top 4 (empid)
+--  FROM [sql pract].[dbo].[emplyoeesalary]
+
+
+SELECT distinct (empid)
+  FROM [sql pract].[dbo].[emplyoeesalary] 
+
+  SELECT avg(salary)
+  FROM [sql pract].[dbo].[emplyoeesalary]
+
+  SELECT max(salary)
+  FROM [sql pract].[dbo].[emplyoeesalary]
+
+  SELECT min(salary)
+  FROM [sql pract].[dbo].[emplyoeesalary]
+
+  SELECT count(salary)
+  FROM [sql pract].[dbo].[emplyoeesalary]
+
+  SELECT min(salary) as minimunsal
+  FROM [sql pract].[dbo].[emplyoeesalary]
+
+
+
