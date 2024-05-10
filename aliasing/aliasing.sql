0c8f95ab5e5d2d253515939b3d99fc7fa461076b
prerantej <prerantejkvr@gmail.com>
2024-05-10 21:03:01 +0530

    aliasing added

---
+++ aliasing/aliasing.sql
@@ -0,0 +1,10 @@
+--select det.empid,sal.salary from emplyoeeDetails as det
+--join emplyoeesalary as sal
+--on det.empid=sal.empid
+
+
+select empid as id from emplyoeeDetails
+
+select empid id from emplyoeeDetails
+
+--" "  can be used as AS
